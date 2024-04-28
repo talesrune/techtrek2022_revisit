cdca5700524aa1c38092289f605693fc155c7dc3
TW <ltwei96@gmail.com>
2024-04-29 01:16:05 +0800

    Added switch function and working exchange rate

--- frontend/src/components/Swap.tsx
+++ frontend/src/components/Swap.tsx
@@ -1,12 +1,14 @@
 import MiniDrawer from './MiniDrawer'
 import Grid from '@mui/material/Grid'
 import { Box, Button, Paper, Typography, Toolbar } from '@mui/material'
+import { useState } from 'react'
 // import { yellow } from "@mui/material/colors";
 import SwapTextField from './SwapTextField';
 import axios from 'axios'
 import {
   useQuery
 } from '@tanstack/react-query'
+import { useForm, FormProvider} from 'react-hook-form';
 
 const getRates = async () => {
   try{
@@ -23,10 +25,14 @@ const getRates = async () => {
   }
 }
 
+type FormValues = {
+  fromAmt: Number
+  toAmt: Number
+}
+
 const Swap = () => {
-  // const [from, setFrom] = useState<string>('')
-  // const [to, setTo] = useState<string>('')
-  // const hello = 'world'
+  const [from, setFrom] = useState<string>('')
+  const [to, setTo] = useState<string>('')
 
   const {data:rateData} = useQuery({ queryKey: ['getrates'], queryFn: getRates })
   // console.log(rateData.dataurrency)
@@ -42,6 +48,17 @@ const Swap = () => {
 
   // console.log(base_currency)
   // console.log(exchange_currency)
+
+  const methods = useForm<FormValues>( //pass the useForm hook to a constant
+  {
+      defaultValues:{
+          fromAmt: 0,
+          toAmt:0
+      }
+  })
+  const {getValues, setValue} = methods //bring out the object properties from useForm hook
+
+  console.log(getValues())
   
   return (
     <div style={{display:'flex'}}>
@@ -52,14 +69,45 @@ const Swap = () => {
               <Typography>Swap</Typography>
           </Grid>
           <Grid item xs={12}>
+            <FormProvider {...methods} > 
             <Paper elevation={0} sx={{ maxWidth:'448px', height:'17em', backgroundColor:'rgb(48,66,86)', borderRadius:'15px'}}>
-              <SwapTextField label={'You are paying'} base={base_currency} exchange={exchange_currency}/>
-              <SwapTextField label={'To Receive'} base={base_currency} exchange={exchange_currency}/>
+              <SwapTextField label={'You are paying'} 
+                type={'from'}
+                base={base_currency} 
+                exchange={exchange_currency} 
+                from={from} 
+                setFrom={setFrom}
+                to={to}
+                setTo={setTo}
+                rateData={rateData}
+                />
+               <Box textAlign='center'><Button sx={{color:'rgb(255,255,239)'}} size={'small'} 
+                  onClick={()=>{
+                    let tempTo = to
+                    setTo(from)
+                    setFrom(tempTo)
+
+                    let tempToAmt = getValues('toAmt')
+                    setValue('toAmt', getValues('fromAmt'))
+                    setValue('fromAmt', tempToAmt)
+                  }}>Switch</Button>
+              </Box>
+              <SwapTextField label={'To Receive'} 
+                type={'to'}
+                base={base_currency} 
+                exchange={exchange_currency} 
+                from={from} 
+                setFrom={setFrom}
+                to={to}
+                setTo={setTo}
+                rateData={rateData}
+                />
               <Toolbar  sx={{ justifyContent: "space-between" }}>   
                 <div/>
                 <Button variant='contained'>SWAP</Button> 
               </Toolbar >
             </Paper>
+            </FormProvider>
           </Grid>         
         </Grid>
     </Box>
--- frontend/src/components/SwapTextField.tsx
+++ frontend/src/components/SwapTextField.tsx
@@ -1,7 +1,7 @@
 import Grid from '@mui/material/Grid'
 import { NativeSelect, Paper, TextField, Typography, TextFieldProps, inputBaseClasses } from '@mui/material'
-import { useState } from 'react'
 import { yellow } from "@mui/material/colors";
+import { useFormContext } from 'react-hook-form';
 
 const InputProps: TextFieldProps["InputProps"] = {
   disableUnderline: true,
@@ -30,44 +30,110 @@ const InputProps: TextFieldProps["InputProps"] = {
   },
 };
 
-const SwapTextField = ({label, base, exchange}:{label: string, base:Set<string>, exchange:Set<string>}) => {
+
+const SwapTextField = ({label, type, base, exchange, from, setFrom, to, setTo, rateData}:
+  {label: string, type:string, base:Set<string>, exchange:Set<string>, from: string, setFrom:any, to: string, setTo:any, rateData:any}) => {
   // const curList = ['SGD','USD']
-  const [from, setFrom] = useState<string>('')
-  // const [to, setTo] = useState<string>('')
 
   const cur1 = Array.from(base.values());
   const cur2 = Array.from(exchange.values());
   const curList = cur1.concat(cur2)
-  console.log(curList)
+  // console.log(curList)
 
+  const {register, setValue} = useFormContext()
+
+  const checkOptions = () => {
+    if (type === 'from') {          //to has something and not SGD
+      // const ind_cur = (to !== '' && !cur1.includes(to))? cur1.concat([to]) : cur1
+      if(cur1.includes(from)  || cur2.includes(to)){
+        return cur1.map((item)=>{
+          return <option key={new Date().getTime() + item} value={item}>{item}</option>
+        })
+      } else if (cur2.includes(from) || cur1.includes(to)) {
+        return cur2.map((item)=>{
+          return <option key={new Date().getTime() + item} value={item}>{item}</option>
+        })
+      }      
+    } else if (type === 'to') {
+      if(cur1.includes(to)  || cur2.includes(from)){
+        return cur1.map((item)=>{
+          return <option key={new Date().getTime() + item} value={item}>{item}</option>
+        })
+      } else if (cur2.includes(to)  || cur1.includes(from)) {
+        return cur2.map((item)=>{
+          return <option key={new Date().getTime() + item} value={item}>{item}</option>
+        })
+      }
+    }
+    return curList.map((item)=>{
+      return <option key={new Date().getTime() + item} value={item}>{item}</option>
+    })
+  }
+
+  const checkRates = (inputAmt:number) => {
+    rateData?.data.map((obj:any)=> {
+      if (obj.base_currency === from && obj.exchange_currency === to) {
+        if(type === 'from') {
+          console.log('setting To amt based on From: ' + inputAmt + ' and ' + obj.rate)
+          setValue('toAmt', inputAmt*obj.rate)
+        } else {
+          console.log('setting From amt based on To: ' + inputAmt + ' and ' + obj.rate)
+          setValue('fromAmt', inputAmt/obj.rate)
+
+        }
+      } else if (obj.base_currency === to && obj.exchange_currency === from) {
+        if(type === 'from') {
+          console.log('setting to amt based on From (2): ' + inputAmt + ' and ' + obj.rate)
+          setValue('toAmt', inputAmt/obj.rate)
+        } else {
+          console.log('setting From amt based on To (2): ' + inputAmt + ' and ' + obj.rate)
+          setValue('fromAmt', inputAmt*obj.rate)
+        }
+      }
+    })
+  }
   return (
     <div style={{display:'flex'}}>
             <Grid container spacing={0}>
             <Grid item xs={0.5}/>
             <Grid item xs={11}>
-              <Typography sx={{paddingTop:'1em', color:'rgb(255,255,239)'}}>{label}</Typography>
+              <Typography sx={{paddingTop:(type === 'from')?'1em':'0em', color:'rgb(255,255,239)'}}>{label}</Typography>
               <Paper elevation={0} sx={{width:'100%', backgroundColor:'rgb(25,35,45)', borderRadius:'10px'}}>
                 <Grid container spacing={0}>
                   <Grid item xs={4}>
                     <NativeSelect
                       variant='standard'
-                      value={from}
+                      value={(type === 'from') ? from : to}
                       sx={{borderRadius:'10px', height:'50%', marginLeft:'1em', marginTop:'1em', color:'rgb(255,255,239)', backgroundColor:'rgba(254,35,45,0.4)', fontSize:'16px', 'option':{color:'rgb(0,0,0)'}}}
                       onChange={(e:any)=>{
-                        setFrom(e.target.value)
+                        if (type === 'from') {
+                          setFrom(e.target.value)
+                        } else {
+                          setTo(e.target.value)
+                        }
                       }}
                     >
                       <option key={new Date().getTime() + 'select'} value={''}>Choose</option>
-                      {
-                        curList.map((item)=>{
-                          return <option key={new Date().getTime() + item} value={item}>{item}</option>
-                        })
-                      }
+                      {checkOptions()}
                     </NativeSelect>
                   </Grid>
                  
                   <Grid item xs={8}>
                     <TextField 
+                      {...(type==='from') ? 
+                      {...register('fromAmt', {
+                          onChange: (e) => {
+                            checkRates(e.target.value)
+                            console.log('from moving')
+                          }
+                        })
+                      }:{...register('toAmt', {
+                          onChange: (e) => {
+                            checkRates(e.target.value)
+                            console.log('to moving')                            
+                          }
+                        })
+                      }}
                       variant='standard'
                       fullWidth
                       sx={{
